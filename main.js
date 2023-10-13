649c221158454cb3098b39c1d083dccf33c2c6a3
Kwony7 <126170052+Kwony7@users.noreply.github.com>
2023-10-13 09:36:05 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ var input = document.getElementById("input");
 var moneyVal = document.getElementById("moneyVal");
 var money = 100;
 var boxes = document.getElementById("boxes");
+var gamble2panel = document.getElementById("gamble2panel");
+var panel = false;
+var input2Val = document.getElementById("input2").value;
+var output = document.getElementById("output");
 
 function textColor(){
     if(money >= 1000000){
@@ -28,7 +32,23 @@ function borderthing(){
     box2.classList.remove("boxBorder");
     box3.classList.remove("boxBorder");
 }
+function gamble2(){
+    if(input2Val > 9 || input2Val < 0){
+        alert("Please type a value between 0 and 9");
+        return;
+    }
+    var outputVal = Math.floor(Math.random() * 9);
 
+    output.innerHTML = outputVal;
+    if(outputVal == input2Val){
+        money = money * 2;
+        moneyVal.innerHTML = money;
+    }
+    if(outputVal != input2Val){
+        money = money / 2;
+        moneyVal.innerHTML = money;
+    }
+}
 
 function clic(){
     if(money < input.value){
@@ -63,3 +83,20 @@ function clic(){
         borderthing();
     }
 }
+
+function onoff(){
+    if(panel == false){
+        gamble2panel.classList.remove("invisible");
+        gamble2panel.classList.add("visible");
+        panel = true;
+        return;
+    }
+    if(panel == true){
+        gamble2panel.classList.add("invisible");
+        gamble2panel.classList.remove("visible");
+        panel = false;
+        return;
+    }
+}
+gamble2panel.classList.add("invisible");
+gamble2panel.classList.remove("visible");
